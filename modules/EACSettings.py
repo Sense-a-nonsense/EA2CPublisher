0d08657f4efddb1adbacc3c3cade6235b3da0cfa
Mikhail Shilov <92148660+Sense-a-nonsense@users.noreply.github.com>
2021-11-11 16:50:29 +0300

    Add files via upload

--- modules/EACSettings.py
+++ modules/EACSettings.py
@@ -16,7 +16,7 @@ class UserSettings(object):
             for line in settings_file:
                 setting = line.split(':', 1)
                 settings[setting[0].strip(' ')] = setting[1].strip(' \n')
-        self.url = settings['url']
+        self.url = settings['url'].rstrip('/')
         self.PAT = settings['PAT']
         self.space = settings['requirements_space']
         self.publish_path = str(settings['publish_path']).strip('\\')
